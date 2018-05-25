b739fe871b6fe1ef13aaf25c425e511a3336df95
s2h <ssh92kr0410@naver.com>
2018-05-26 02:19:17 +0900

    BTC 작업중

--- BitCoinTicker/Controller/BTCViewController.swift
+++ BitCoinTicker/Controller/BTCViewController.swift
@@ -7,13 +7,59 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class BTCViewController: UIViewController {
+    
+    let coinURL = "https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=btc_krw"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getBitcoinData(url: coinURL)
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    func getBitcoinData(url: String) {
+        
+        Alamofire.request(url, method: .get).responseJSON { (response) in
+            if response.result.isSuccess {
+                
+                //print("Success")
+                
+                let bitcoinJSON : JSON = JSON(response.result.value)
+                
+                self.updateBitcoinData(json: bitcoinJSON)
+                
 
-      
+                print(bitcoinJSON)
+                
+            } else {
+                print("Error: \(String(describing: response.result.value))")
+                
+            }
+        }
+    }
+    
+    /***************************************************************/
+    //MARK: - JSON Parsing
+    
+    func updateBitcoinData(json : JSON) {
+        
+        let bitcoinResult = json["data"]["opening_price"].intValue
+        let bitcoinResult2 = json["data"]["closing_price"].intValue
+        
+        
+        
+//        currencyLabel.text = String(bitcoinResult)
+//        priceLabel.text = String(bitcoinResult2)
+        
+        
     }
 
    
